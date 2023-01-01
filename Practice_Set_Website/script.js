3e8f2738e8898cc91f37553191ff0cded3761979
Suyog Kulkarni <suyog_kulkarni@persistent.com>
2023-01-01 17:26:41 +0530

    Added few more concepts

---
+++ Practice_Set_Website/script.js
@@ -0,0 +1,10 @@
+alert("Your site is up and running, Congratulations !!")
+let a = Number.parseInt(prompt("Please enter a number : "))
+let result = a * a;
+let consent = confirm("Do you want to write results to a document ??")
+if (consent) {
+    document.write("Value of Square of Given Number ", a, " is : ", result)
+}
+else {
+    document.write("Allow me write to a document")
+}
